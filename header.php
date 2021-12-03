c1510adcde13a072c5082f039d7cc7d083ef6796
carla-piazza1 <91873098+carla-piazza1@users.noreply.github.com>
2021-12-03 10:09:14 +0100

    header fini condition et liens à faire

--- header.php
+++ header.php
@@ -4,12 +4,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="header.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
-<body>
-    
+<body class='body_header'>
+  <main>
+    <div class="wrap">
+    <span class="decor"></span>
+    <nav>
+      <ul class="primary">
+        <li>
+          <a href="">Accueil</a>
+        </li>
+        <li>
+          <a href="">article</a>
+          <ul class="sub">
+            <li><a href="">les fusées</a></li>
+            <li><a href="">la galaxie</a></li>
+            <li><a href="">la nasa</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">
+              <!-- <div class='icon'> -->
+              <i class="fa fa-user-circle" aria-hidden="true"></i>
+            <!-- </div> -->
+          <ul class="sub">
+            <li><a href="">connexion</a></li>
+            <li><a href="">inscription</a></li>
+            <li><a href="">Crow</a></li>
+          </ul>  
+        </li>
+    </nav>
+    </div>
+  </main>
 </body>
 </html>
