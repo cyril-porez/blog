d7cce4626ca0ceeac5982a6e46df43d55a400f4a
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2021-12-09 15:58:54 +0100

    mise au propre de la branche revoir l'indentation

--- profil.php
+++ profil.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-$bdd=mysqli_connect('localhost','root','root','blog');
-mysqli_set_charset($bdd,'utf8');
+    session_start();
+    $bdd=mysqli_connect('localhost','root','root','blog');
+    mysqli_set_charset($bdd,'utf8');
 
 ?>
 
@@ -16,10 +16,9 @@ mysqli_set_charset($bdd,'utf8');
 <body>
 <main>
 <?php
- var_dump($_SESSION["user"]);
- var_dump($_SESSION);
- var_dump($_POST);
-
+    var_dump($_SESSION["user"]);
+    var_dump($_SESSION);
+    var_dump($_POST);
 
     if(isset($_POST["editer"]))
     {
@@ -61,49 +60,40 @@ mysqli_set_charset($bdd,'utf8');
             
                         <input type="submit" name="editer" value="Editer mon profil">
                 </form>
+                <?php
+                    $requete=mysqli_query($bdd,"SELECT * FROM articles");
+                    $array = [];
+                    $i = 0;
 
-                    <?php
+                    while($result = mysqli_fetch_assoc($requete))
+                    {
+                        $array[] = $result; 
+                    }
+                        echo 
+                                "
+                                    <div style='display:flex; flex-direction:column;background-color:red;'>
+                                        <h1>".$array[$i]['titre']."<h1/>
+                                        <p>".$array[$i]['description']."</p>
+                                        <img src='".$array[$i]['image']."'>
+                                        <p style='color:blue;'>".$array[$i]['date']."</p>
+                                    </div>
+                                ";
 
-$requete=mysqli_query($bdd,"SELECT * FROM articles");
-$array = [];
-$i = 0;
-
-while($result = mysqli_fetch_assoc($requete))
-{
-    $array[] = $result; 
-}
-echo 
-"
-<div style='display:flex; flex-direction:column;background-color:red;'>
-<h1>".$array[$i]['titre']."<h1/>
-<p>".$array[$i]['description']."</p>
-<img src='".$array[$i]['image']."'>
-<p style='color:blue;'>".$array[$i]['date']."</p>
-
-</div>
-";
-
-
-for($i = 0; $i < 3; $i++)
-{
-    // echo '<pre>';
-    // var_dump($array);
-    // echo '</pre>';
+                    for($i = 0; $i < 3; $i++)
+                    {
+                        // echo '<pre>';
+                        // var_dump($array);
+                        // echo '</pre>';
     
-            echo 
-            "
-            <a href='profil.php?id=".$array[$i]['id']."' style='display:flex; flex-direction:column; background-color:grey; border: 1px solid blue'>
-            <h1>".$array[$i]['titre']."<h1/>
-            <img src='".$array[$i]['image']."'>
-            </a>
-            ";
-        }
-
-
-                    ?>
-
-
-
+                        echo 
+                             "
+                                <a href='profil.php?id=".$array[$i]['id']."' style='display:flex; flex-direction:column; background-color:grey; border: 1px solid blue'>
+                                <h1>".$array[$i]['titre']."<h1/>
+                                <img src='".$array[$i]['image']."'>
+                                </a>
+                            ";
+                    }
+        ?>
         </main>
         
 </body>
