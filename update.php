345194725be5aaa8884e4b1e9d296fbe85ddbb5f
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2021-12-15 15:41:10 +0100

    possibilite de modifié les éléments d'un utilisateur de manière séparé

--- update.php
+++ update.php
@@ -1,54 +1,81 @@
 <?php
     $connex = mysqli_connect("localhost", "root", "", "blog");
+    mysqli_set_charset($connex, 'utf8');
+
     $user = $_GET["update"];
-    $requete = mysqli_query($connex, "SELECT * FROM utilisateurs WHERE id = '$user'");
+    $requete = mysqli_query($connex, "SELECT utilisateurs.id, login, email, id_droits, droits.nom as droits from utilisateurs inner join droits on  id_droits = droits.id WHERE utilisateurs.id = '$user'");
     $infos = mysqli_fetch_all($requete, MYSQLI_ASSOC);
-    $error = "";
-    var_dump($infos);
-    echo $user;
 
-    if (!empty($_POST["login"]) && !empty($_POST["email"]) && !empty($_POST["droits"]) && !empty($_POST["password"]) && !empty($_POST["confirmPassword"])) {
-        $login = $_POST["login"];
-        $email = $_POST["email"];
-        $droits = $_POST["droits"];
-        $password = $_POST["password"];
-        $confirmPassword = $_POST["confirmPassword"];
-        $requete2 = mysqli_query($connex, "SELECT login, email FROM utilisateurs WHERE login = '$login' and email = '$email'");
-        $verif = mysqli_fetch_all($requete2, MYSQLI_ASSOC);
-        var_dump($verif);
+    $errorLogin = "";
+    $errorEmail = "";
+    $errorPassword = "";
+    $errorDroit = "";
+    
+    if (!empty($_POST["login"])) {
+        $login = $_POST["login"];       
        // $recupLogin = $verif[0]['login'];
-
         $requete3 = mysqli_query($connex, "SELECT login FROM utilisateurs WHERE login = '$login'");
         $verifLogin = mysqli_fetch_all($requete3, MYSQLI_ASSOC);
         var_dump($verifLogin);
+
+        if (count($verifLogin) == 0) {
+            $update = mysqli_query($connex, "UPDATE utilisateurs set login = '$login' WHERE id = '$user'");
+            header("Refresh:0");
+        }
+        else {
+            $errorLogin = "* Ce login est déjà pris";
+        }
+    }
+    else if (!empty($_POST["email"])) {
+        $email = $_POST["email"];
+        
         $requete4 = mysqli_query($connex, "SELECT email FROM utilisateurs WHERE email = '$email'");
         $verifEmail = mysqli_fetch_all($requete4, MYSQLI_ASSOC);
         var_dump($verifEmail);
-        if (count($verifLogin) == 0) {
-            if (count($verifEmail) == 0) {
-                if ($password == $confirmPassword) {
-                    $hash = password_hash($password, PASSWORD_ARGON2I);
-                    $update = mysqli_query($connex, "UPDATE utilisateurs set login = '$login', password = '$email', id_droits = '$droits', password = '$hash' WHERE id = '$user'");
-                    //header("Refresh:0");
-                    var_dump($update);
-                }
-                else {
-                    $error = "* problème entre le mot de passe et la confirmation mot de passe";
-                }
-            }
-            else {
-                $error = "* un compte avec cet email existe déjà";
-            }
+
+        if (count($verifEmail) == 0) {            
+            $update = mysqli_query($connex, "UPDATE utilisateurs set email = '$email' WHERE id = '$user'");
+            header("Refresh:0");
         }
         else {
-            $error = "* Ce login est déjà pris";
+            $errorEmail = "* un compte avec cet email existe déjà";
         }
     }
+    else if (!empty($_POST["id_droits"])) {
+        $droit = $_POST["id_droits"];
+        
+        if ($droit == "1" || $droit == "42" || $droit == "1337") {
+            $update = mysqli_query($connex, "UPDATE utilisateurs set id_droits = '$droit' where id = '$user'");
+            header("Refresh: 0");
+        }
+        else {
+            $errorDroit = "les droits que vous donnez n'existe pas";
+        }
+    }
+
+    else if (!empty($_POST["password"]) && !empty($_POST["confirmPassword"])) {
+        $password = $_POST["password"];
+        $confirmPassword = $_POST["confirmPassword"];
+
+        if ($password == $confirmPassword) {
+            $hash = password_hash($password, PASSWORD_ARGON2I);
+            $update = mysqli_query($connex, "UPDATE utilisateurs set password = '$hash' where id = '$user'");
+        }
+        else {
+            $errorPassword = "* problème entre le mot de passe et la confirmation mot de passe";
+        }
+    }
+    
 
     if (isset($_POST["back"])) {
         header("Location: admin.php");
     }
+
+        //, password = '$email', id_droits = '$droits', password = '$hash'
+
+       // 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -63,17 +90,58 @@
 
     </header>
     <main>
-        <form action="" method="post">
-            <input type="text" name="login" placeholder="login" value=<?php echo $infos[0]['login']; ?>>
-            <input type="text" name="email" placeholder="email" value=<?php echo $infos[0]['email']; ?>>
-            <input type="text" name="droits" placeholder="droits" value=<?php echo $infos[0]['id_droits']; ?>>
-            <input type="text" name="password"  placeholder="password">
-            <input type="text" name="confirmPassword" placeholder=confirmPassword>
-            <input type="submit" name="update" value="modifier">
-            <input type="submit" name="back" value="retour">
-            <?php
-                echo $error;
-            ?>
+            <fieldset>
+                <legend>
+                    <p>login</p>
+                </legend>
+                <form action="" method="post">
+                    <input type="text" name="login" placeholder="login" value=<?php echo $infos[0]['login']; ?>>
+                    <input type="submit" name="update_login" value="modifier">
+                    <?php
+                        echo $errorLogin;
+                    ?>
+                </form>
+            </fieldset>            
+            <fieldset>
+                <legend>
+                    <p>email</p>
+                </legend>
+                <form action="" method="post">
+                    <input type="text" name="email" placeholder="email" value=<?php echo $infos[0]['email']; ?>>
+                    <input type="submit" name="update_email" value="modifier">
+                    <?php
+                        echo $errorEmail;
+                    ?>
+                </form>
+            </fieldset>
+            <fieldset>
+                <legend>
+                    <p>Droits utilisateur</p>
+                </legend>
+                <form action="" method="post">
+                    <input type="text" name="id_droits" placeholder="id_droits" value=<?php echo $infos[0]['id_droits']; ?>>
+                    <!--<input type="text" name="droits" placeholder="droits" value=<?php echo $infos[0]['droits']; ?>>-->
+                    <input type="submit" name="update_droits" value="modifier">
+                    <?php
+                        echo $errorDroit;
+                    ?>
+                </form>
+            </fieldset>
+            <fieldset>
+                <legend>
+                    Mot de passe
+                </legend>
+                <form action="" method="post">
+                    <input type="text" name="password"  placeholder="password">
+                    <input type="text" name="confirmPassword" placeholder=confirmPassword>
+                    <input type="submit" name="update_password" value="modifier">
+                    <?php
+                        echo $errorPassword;
+                    ?>
+                </form>
+            </fieldset>
+        <form action="" method="post">     
+            <input type="submit" name="back" value="retour">         
         </form>
     </main>
     <footer>
