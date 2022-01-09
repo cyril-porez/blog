9c60add7ba3066473eb6f146f24876daf61e3a91
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-01-09 22:26:47 +0100

    like dislike opérationel

--- article.php
+++ article.php
@@ -15,21 +15,46 @@
     
     $requete2 = mysqli_query($connex, "SELECT articles.id, article, nom, date, login  from articles inner join utilisateurs inner join categories on articles.id_utilisateur = utilisateurs.id and articles.id_categorie = categories.id where articles.id = '$recupArticle'");
     $articles = mysqli_fetch_all($requete2, MYSQLI_ASSOC);
-    var_dump($articles);
+    //var_dump($articles);
     if (isset($_POST["message"])) {
         $msg = $_POST["message"];
         $requete = mysqli_query($connex, "INSERT into commentaires (commentaire, id_article, id_utilisateur) values ('$msg', '$recupArticle', '$idUser')");
     }
 
+    //Requete permettant de récupérer l'id d'un like dislike 
+    
+
+    $requete4 = mysqli_query($connex, "SELECT count(etat_like) as etat_like from intermediaire_article_like where id_utilisateur = '$user' and id_article = '$recupArticle' and etat_like = '1'");
+    $select_etat_like = mysqli_fetch_all($requete4, MYSQLI_ASSOC);
+    $etat_like = $select_etat_like[0]["etat_like"];
+
+    $requete4 = mysqli_query($connex, "SELECT count(dislike) as dislike from intermediaire_article_like where id_utilisateur = '$user' and id_article = '$recupArticle' and dislike ='1'");
+    $select_etat_dislike = mysqli_fetch_all($requete4, MYSQLI_ASSOC);    
+    $etat_dislike = $select_etat_dislike[0]["dislike"];
+    
     if (isset($_POST["like"])) {
-        $like = $_POST["like"];
-        $requete_like = mysqli_query($connex, "INSERT into intermediaire_article_like (id_article, id_utilisateur, etat_like, dislike) values ('$recupArticle', '$user', '1', '0')");
-        header("Refresh: 0");
-        var_dump($requete_like);
+        echo "bob";        
+        if ($etat_like == "0" && $etat_dislike == "1") {            
+            $requete_update_like = mysqli_query($connex, "UPDATE intermediaire_article_like SET id_article = '$recupArticle' , id_utilisateur = '$user', etat_like =  '1', dislike = '0' where id_article = '$recupArticle' and id_utilisateur = '$user'");
+        }
+        else if ($etat_like == "1" && $etat_dislike == "0") {
+            $requete_delete_like = mysqli_query($connex, "DELETE from intermediaire_article_like where id_article ='$recupArticle' and id_utilisateur = '$user'");
+
+        }
+        else {
+            $requete_like = mysqli_query($connex, "INSERT into intermediaire_article_like (id_article, id_utilisateur, etat_like, dislike) values ('$recupArticle', '$user', '1', '0')");
+        }
     }
     else if (isset($_POST["dislike"])) {
-        $dislike = $_POST["dislike"];
-        $requete_dislike = mysqli_query($connex, "UPDATE into intermediaire_article_like (id_article, id_utilisateur, etat_like, dislike) values ('$recupArticle', '$user', '0', '1')");
+        if ($etat_like == "1" && $etat_dislike == "0") {
+            $requete_dislike = mysqli_query($connex, "UPDATE intermediaire_article_like SET id_article = '$recupArticle' , id_utilisateur = '$user', etat_like =  '0', dislike = '1' where id_article = '$recupArticle' and id_utilisateur = '$user'");
+        }
+        else if ($etat_like == "0" && $etat_dislike == "1") {
+            $requete_delete_like = mysqli_query($connex, "DELETE from intermediaire_article_like where id_article ='$recupArticle' and id_utilisateur = '$user'");
+        }
+        else {
+            $requete_dislike = mysqli_query($connex, "INSERT into intermediaire_article_like (id_article, id_utilisateur, etat_like, dislike) values ('$recupArticle', '$user', '0', '1')");
+        }
     }
 ?>
 <main>
@@ -45,12 +70,15 @@
                 $requeteLike = mysqli_query($connex, "SELECT count(etat_like) as nbr_like from intermediaire_article_like where id_article = '$id_article' and etat_like = 1");
                 $nbr_like = mysqli_fetch_all($requeteLike, MYSQLI_ASSOC);
                 var_dump($nbr_like);
+                $requeteLike = mysqli_query($connex, "SELECT count(dislike) as nbr_dislike from intermediaire_article_like where id_article = '$id_article' and dislike = 1");
+                $nbr_dislike = mysqli_fetch_all($requeteLike, MYSQLI_ASSOC);
             ?>
             <p><?php echo $nbr_like[0]["nbr_like"] ?></p>
             <form action="" method="post">
                 <button name="like"><img src="images/like.jpg" class="logo" alt="like"></button>
                 <button name="dislike"><img src="images/deslike.png" class="logo" alt="dislike"></button>
             </form>
+            <p><?php echo $nbr_dislike[0]["nbr_dislike"] ?></p>
         </div>
         <div>
             <h1>Commentaire</h1>
