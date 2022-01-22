906a23260e2e74eda96acc950ada5740fead92e0
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-01-23 00:33:42 +0100

    update password et email profil.php

--- profil.php
+++ profil.php
@@ -1,35 +1,56 @@
 <?php
     session_start();
     require ('bdd.php');
-    var_dump($_SESSION["user"]);
+    
     $title = 'Profil';
     $userConnect = $_SESSION["user"][0]["login"];
-    //echo $userConnect;
+   
     $requete = mysqli_query($connex, "SELECT * FROM utilisateurs WHERE login = '$userConnect'");
     $infoUser = mysqli_fetch_all($requete, MYSQLI_ASSOC);
-    var_dump($infoUser);
-    $error_log = "";
-    $error = "";
+    
+    $errorLog = "";
+    $errorEmail = "";
+    $errorPass = "";
     
     if (!empty($_POST["login"])) {
         $login = $_POST["login"];
         $requete = mysqli_query($connex, "SELECT * FROM utilisateurs WHERE login = '$login'");
         $verifLogin = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+        
         if (count($verifLogin) == 0) {
-                $update = mysqli_query($connex, "UPDATE utilisateurs SET login = '$login' WHERE login ='$userConnect'");
-                $_SESSION["user"] = [
-                                        0 => ['id' => $infoUser[0]['id'], 
-                                              'login' => $login, 
-                                              'password' => $infoUser[0]['password'],
-                                              'email' => $infoUser[0]['email'],
-                                              'id_droits' => $infoUser[0]['id_droits'],
-                                             ]
+            $update = mysqli_query($connex, "UPDATE utilisateurs SET login = '$login' WHERE login ='$userConnect'");
+            $_SESSION["user"] = [
+                                    0 => ['id' => $infoUser[0]['id'], 
+                                          'login' => $login, 
+                                          'password' => $infoUser[0]['password'],
+                                          'email' => $infoUser[0]['email'],
+                                          'id_droits' => $infoUser[0]['id_droits'],
+                                         ]
                                     ];
                // header("refresh: 0");
-                var_dump($_SESSION["user"]);
         }
         else {
             $error_log = "* Ce login existe déjà";
+        }
+    }
+    else if (!empty($_POST["email"])) {
+        $email = $_POST["email"];
+        $requete = mysqli_query($connex, "SELECT * FROM utilisateurs WHERE email = '$email'");
+        $verifEmail = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+
+        if (count($verifEmail) == 0) {
+            $update = mysqli_query($connex, "UPDATE utilisateurs SET email = '$email' WHERE login ='$userConnect'");
+            $_SESSION["user"] = [
+                0 => ['id' => $infoUser[0]['id'], 
+                      'login' => $infoUser[0]['login'], 
+                      'password' => $infoUser[0]['password'],
+                      'email' => $email,
+                      'id_droits' => $infoUser[0]['id_droits'],
+                     ]
+            ];
+        }
+        else {
+            $errorEmail = "* Un compte avec cet email existe déjà";
         }
     }
     else if (!empty($_POST["password"])) {
@@ -40,13 +61,15 @@
             $update = mysqli_query($connex, "UPDATE utilisateurs SET password = '$passHash' WHERE login ='$userConnect'");
             header("refresh: 0");
         }
+        else {
+            $errorPass = "* Les deux mots de passes ne correspondent pas";
+        }
 
-    }
-    else if (isset($_POST["login"])) {
-        $error_log = "* oublis dans les champs";
-    }
-    else if (isset($_POST["password"])) {
-        $error = "* oublis dans les champs";
+    }   
+    else if (isset($_POST["email"]) || isset($_POST["password"]) || isset($_POST["confirmPassword"]) || isset($_POST["login"])) {
+        $errorLog = "* oublis dans l'un des champs";
+        $errorPass = "* oublis dans l'un des champs";
+        $errorEmail = "* oublis dans l'un champs";
     }
 ?>
 <body>
@@ -97,7 +120,19 @@
                     <input type="text" name="login" placeholder="login" class="champs" value=<?php echo $_SESSION["user"][0]["login"]; ?>>
                     <input type="submit" name="update_login" value="editer" class="bouton">
                     <?php
-                        echo "<p>$error_log</p>";
+                        echo "<p>$errorLog</p>";
+                    ?>
+                </form>
+            </fieldset>
+        </div>
+        <div id="formu_login">
+            <fieldset class="entete">
+                <legend><p class="titre"><b>Email</b></p></legend>
+                <form action="profil.php" method="post" id="formEmail">
+                    <input type="text" name="email" placeholder="email" class="champs" value=<?php echo $_SESSION["user"][0]["email"]; ?>>
+                    <input type="submit" name="update_email" value="editer" class="bouton">
+                    <?php
+                        echo "<p>$errorEmail</p>";
                     ?>
                 </form>
             </fieldset>
@@ -110,7 +145,7 @@
                     <input type="text" name="confirmPassword" placeholder="confirmPassword" id="confirmPass" class="champs">
                     <input type="submit" name="update" value="editer" class="bouton">
                     <?php
-                        echo "<p>$error</p>";
+                        echo "<p>$errorPass</p>";
                     ?>
                 </form>
             </fieldset>
