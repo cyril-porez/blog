1641f9273bb2dd7b8f491a2b847014ad65fa35ec
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-01-24 10:56:41 +0100

    push du projet restructuration du html et possibilité d' accéder au  article par catégorie pour la navbar

--- creer-article.php
+++ creer-article.php
@@ -4,13 +4,18 @@
     
     $title = 'Créer article';
     
-    $id_user = $_SESSION["user"][0]["id"];
-    $request= mysqli_query($connex, "SELECT * FROM categories");
-    $fetch = mysqli_fetch_all($request, MYSQLI_ASSOC);
-    if (isset($_POST["text_article"]) && isset($_POST["categorie"])) {
-        $txt_article = $_POST["text_article"];        
-        $id_cat= $_POST["categorie"];        
-        $request2= mysqli_query($bdd, "INSERT INTO articles (article, id_utilisateur, id_categorie) VALUES ('$txt_article', '$id_user', '$id_cat')");   
+    if (!empty($_SESSION)) { 
+        $id_user = $_SESSION["user"][0]["id"];
+        $request= mysqli_query($connex, "SELECT * FROM categories");
+        $fetch = mysqli_fetch_all($request, MYSQLI_ASSOC);
+        if (isset($_POST["text_article"]) && isset($_POST["categorie"])) {
+            $txt_article = $_POST["text_article"];        
+            $id_cat= $_POST["categorie"];        
+            $request2= mysqli_query($bdd, "INSERT INTO articles (article, id_utilisateur, id_categorie) VALUES ('$txt_article', '$id_user', '$id_cat')");   
+        }
+    }
+    else {
+        header("Location: index.php");
     }
 ?>
 
--- favoris.php
+++ favoris.php
@@ -4,7 +4,7 @@
     require ('bdd.php');
     
     $user = $_SESSION['user'][0]['id'];
-    echo $user;
+
     $requete_favoris = mysqli_query($connex, "SELECT articles.id, article, login, nom, date from articles inner join utilisateurs on articles.id_utilisateur = utilisateurs.id inner join categories on articles.id_categorie = categories.id inner join favoris on favoris.id_article = articles.id where favoris.id_utilisateur = '$user'");
     $favoris = mysqli_fetch_all($requete_favoris, MYSQLI_ASSOC);?>
 
