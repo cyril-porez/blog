7d012911eb94aafe0fb0ca84540491368aeaf293
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-01-25 11:53:06 +0100

    tableau des articles

--- admin.php
+++ admin.php
@@ -5,12 +5,16 @@
     $title = 'Admin';
     $error = "";
 
-    $donneesUser = mysqli_query($connex, "SELECT * FROM utilisateurs");
-    $infoUsers = mysqli_fetch_all($donneesUser, MYSQLI_ASSOC);
+    $dataUsers = mysqli_query($connex, "SELECT * FROM utilisateurs");
+    $infoUsers = mysqli_fetch_all($dataUsers, MYSQLI_ASSOC);
     //var_dump($infoUsers);
 
+    $dataArticles = mysqli_query($connex, "SELECT articles.id, article, nom, date, login  from articles inner join utilisateurs on articles.id_utilisateur = utilisateurs.id inner join categories on articles.id_categorie = categories.id ");
+    $infoArticles = mysqli_fetch_all($dataArticles, MYSQLI_ASSOC);
+    //var_dump($infoArticles);
+
     if (!empty($_POST["login"]) && !empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["id_droits"]) && !empty($_POST["confirmPassword"]))
-     {
+    {
         $login = $_POST["login"];
         $email = $_POST["email"];
         $password = $_POST["password"];
@@ -27,7 +31,6 @@
                     $hash = password_hash($password, PASSWORD_ARGON2I);
                     $creeUser = mysqli_query($connex, "INSERT into utilisateurs (login, password,  email, id_droits) VALUES ('$login', '$hash', '$email', '$idDroit')");
                     header("Location: admin.php");
-                    //var_dump($creeUser);
                 }
                 else {
                     $error = "* error password";
@@ -64,6 +67,7 @@
                 <form action="admin.php" method="post">
                     <input class="butt-site" type="submit" name="createUser" value="Créer un utilisateur">
                     <input class="butt-site"  type="submit" name="afficheUser" value="Afficher les utilisateurs">
+                    <input type="submit" name="afficheArticle" value="Afficher les articles">
                     <input type="submit" name="categorie" value="Catégorie">
                     <input class="butt-site" type="submit" name="X" value="X">
                 </form>
@@ -73,7 +77,6 @@
                     <div id='centre_admin'>
                         <div id='form_admin'>
                             <form action='admin.php' method='post'>
-
                                 <input class='connect' type='text' name='login' placeholder='login'></br>
                                 <input class='connect' type='email' name='email' placeholder='defaut@exemple.com'></br>
                                 <input class='connect' type='text' name='id_droits' placeholder='id_droits'></br>
@@ -86,9 +89,7 @@
                     <?php
                 }
                 echo "<p>" . $error . "</p>";
-                if (isset($_POST["afficheUser"]))
-                {
-            ?>
+                if (isset($_POST["afficheUser"])) { ?>
                     <div class="parent-table">
                         <table class="table_cat">
                             <thead>
@@ -105,8 +106,7 @@
                             </thead>
                             <tbody class="td_cat">
                             <?php
-                                foreach($infoUsers as $infoUser)
-                                {
+                                foreach($infoUsers as $infoUser) {
                                     $subrstrPass = substr($infoUser['password'],0 ,10);
                                     echo '  <tr>
                                                 <td id="idAdmin" class="textAdmin">' . $infoUser["id"] . '</td>
@@ -134,6 +134,36 @@
                                 <?php
                             }
                 }
+                else if (isset($_POST["afficheArticle"])) { ?>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>article</th>
+                                <th>nom</th>
+                                <th>date</th>
+                                <th>login</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                foreach($infoArticles as $infoArticle) { ?>
+                                    <tr>
+                                        <td><?php echo $infoArticle['id']; ?></td>
+                                        <td><?php echo $infoArticle['article']; ?></td>
+                                        <td><?php echo $infoArticle['nom']; ?></td>
+                                        <td><?php echo $infoArticle['date']; ?></td>
+                                        <td><?php echo $infoArticle['login']; ?></td>
+                                        <td><button>Supprimer</button></td>
+                                        <td><button>Modifier</button></td>
+                                    </tr>
+                                    <?php
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                  <?php
+                }
                 else if (isset($_POST["categorie"])) {
                     header("Location: categorie.php");
                 }
@@ -145,8 +175,7 @@
     </div>
     </main>
     <footer>
-        <?php require ('footer.php') ?>
+        <?php require ('footer.php'); ?>
     </footer>
 </body>
 </html>
-
